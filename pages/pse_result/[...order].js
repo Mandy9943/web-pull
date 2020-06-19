71f21675824d53bd301cac399ec2f94967e950dd
omar <obello@jidadesarrollos.com>
2020-06-19 10:56:17 -0400

    code

--- pages/pse_result/[...order].js
+++ pages/pse_result/[...order].js
@@ -2,35 +2,36 @@ import React from "react";
 import Head from "next/head";
 import Detail from "../../components/ProductDetail";
 import { getOrderId } from "../../services/ordersApi";
-import {getUser, isAuthenticated, getJwt } from "../../lib/auth";
+import { getUser, isAuthenticated, getJwt } from "../../lib/auth";
 import Header from "./../../components/Common/Header/Header";
 import Footer from "./../../components/Common/Footer/Footer";
+import '../sass/order.css';
 
 
-function Product({data, u_data}) {
-    return (
-    <div>
+
+function Product({ data, u_data }) {
+  return (
+    <div className="order-page">
       <Head>
         <title>Kiero | Resultado de compra </title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Header/>
+      <Header />
 
-        <body>
+      <section className="order-content">
 
-        DATOS DE RESULTADO:
-        Metodo de pago: {data.method_id}<br />
-        Estado del pago: { data.status == 1 ? "APROBADO" : data.status == 6 ? "RECHAZADO" : "PENDIENTE" }
-        
-        
-        <br />
-        Metodo de pago: {data.product.title}<br />
-        Total a pagar: {data.total}<br />
-        SELLERID: {data.seller_id}<br />
-        USERID: {data.user_id}<br />
+        <section className="info">
+          <h3 className="title">DATOS DE RESULTADO</h3>
+          <span className="user-id">USERID: <strong>{data.user_id}</strong></span>
+          <span>SELLERID: <strong>{data.seller_id}</strong></span>
+          <span>Metodo de pago: <strong>{data.method_id}</strong></span>
+          <span>Estado del pago:<strong>{data.status == 1 ? "APROBADO" : data.status == 6 ? "RECHAZADO" : "PENDIENTE"}</strong> </span>
+          <span>Total a pagar: <strong>{data.total} </strong></span>
+          <span className="name-product">Nombre del producto: <strong>{data.product.title}</strong></span>
+        </section>
 
-        </body>
-        <Footer />
+      </section>
+      <Footer />
     </div>
   );
 }
@@ -39,23 +40,23 @@ function Product({data, u_data}) {
 // This gets called on every request
 export async function getServerSideProps(context) {
 
-    // Fetch data from external API
-    let order_id = context.params.order;
+  // Fetch data from external API
+  let order_id = context.params.order;
 
-    let usr = getUser(context);
-    let jwt = getJwt(context);
+  let usr = getUser(context);
+  let jwt = getJwt(context);
 
-    const res = await getOrderId(order_id[0], jwt)
-    const data = await res.data
+  const res = await getOrderId(order_id[0], jwt)
+  const data = await res.data
 
-    console.log(data);
-    const u_data = {
-        user: (usr !== undefined ? usr  : null),
-        authenticated: isAuthenticated(context),
-        jwt: jwt ? jwt : '' 
-    }
+  console.log(data);
+  const u_data = {
+    user: (usr !== undefined ? usr : null),
+    authenticated: isAuthenticated(context),
+    jwt: jwt ? jwt : ''
+  }
 
-    return { props: { data, u_data } }
+  return { props: { data, u_data } }
 }
 
 export default Product
