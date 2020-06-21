2a08e1799a61be0a2cdb728ae1d3d7eec9d7f148
omar <obello@jidadesarrollos.com>
2020-06-21 18:00:02 -0400

    codigo

--- components/News/News.js
+++ components/News/News.js
@@ -15,20 +15,33 @@ import src7 from "../../assets/img/redes/ws.png";
 import logo from "../../assets/img/redes/logo.png";
 
 
+
 class News extends Component {
   render() {
+    let fb = "//www.facebook.com/KieroGroup/";
+    let ig = "//www.instagram.com/kierogroup";
+    let tw = "//twitter.com/KIEROGROUP1";
+    let lk = "//www.linkedin.com/in/kiero-group-15a6a7190/";
+    let pt = "//co.pinterest.com/novedadeskiero/pins/";
+    let yt = "//www.sic.gov.co";
+    let ws = "//www.sic.gov.co";
+    let news = "//www.novedades.kiero.co"
+
     return (
       <div className="News">
         <section className="content">
-          <section className="title"><img src={logo} className="logo" /></section>
+          <section className="title">
+            <Link href={news}><a><img src={logo} className="logo" /></a>
+            </Link>
+          </section>
           <sections className="icons">
-            <Link href="#"><a><img src={src} /></a></Link>
-            <Link href="#"><a><img src={src1} /></a></Link>
-            <Link href="#"><a><img src={src3} /></a></Link>
-            <Link href="#"><a><img src={src7} /></a></Link>
-            <Link href="#"><a><img src={src2} /></a></Link>
-            <Link href="#"><a><img src={src5} /></a></Link>
-            <Link href="#"><a><img src={src4} /></a></Link>
+            <Link href={fb}><a><img src={src} /></a></Link>
+            <Link href={ig}><a><img src={src1} /></a></Link>
+            <Link href={lk}><a><img src={src3} /></a></Link>
+            {/*<img src={src7} />*/}
+            <Link href={tw}><a><img src={src2} /></a></Link>
+            {/*<img src={src5} />*/}
+            <Link href={pt}><a><img src={src4} /></a></Link>
           </sections>
         </section>
       </div>
