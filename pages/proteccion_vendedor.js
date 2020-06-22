8836584ce8023beccdb929157a5af29ae8fa1f17
omar <obello@jidadesarrollos.com>
2020-06-21 22:25:45 -0400

    code

--- pages/proteccion_vendedor.js
+++ pages/proteccion_vendedor.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
-import BuyerProtection from '../components/Help/BuyerProtection/BuyerProtection';
+import SellerProtection from '../components/Help/SellerProtection/SellerProtection';
 
-export default function proteccion_comprador() {
+export default function proteccion_vendedor() {
     return (
         <div>
             <Head>
-                <title>Kiero | Protection de Comprador</title>
+                <title>Kiero | Protection al Vendedor</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <BuyerProtection />
+            <SellerProtection />
         </div>
     )
 }
