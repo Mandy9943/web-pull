d8ae776c7ff836d2cff5cc229a54f87e6fcf6171
omar <obello@jidadesarrollos.com>
2020-07-03 16:35:06 -0400

    code

--- components/ProductsSlider/ProductsSlider.js
+++ components/ProductsSlider/ProductsSlider.js
@@ -29,12 +29,13 @@ export default class ProductsSlider extends Component {
                 }
                 this.setState({ data });
             });
-    }
+        }
 
     render() {
 
 
         let productList = [];
+        let productLista = [];
         let tmpList = [];
         for (let i = 0; i < this.state.data.length; i++) {
             let url = "";
@@ -58,6 +59,22 @@ export default class ProductsSlider extends Component {
             productList.push(<section key={productList.length+1} className="test">{tmpList}</section>)
         }
 
+
+
+        for (let i = 0; i < this.state.data.length; i++) {
+            let url = "";
+            if (this.state.data[i].image) {
+                url = getImgUrl(this.state.data[i].image);
+            } else {
+                url = "https://thednetworks.com/wp-content/uploads/2012/01/picture_not_available_400-300.png";
+            }
+            productLista.push(<ProductCard
+                key={tmpList.length + 1}
+                price={this.state.data[i].price}
+                url={url}
+                product_id={this.state.data[i].product_id}
+                title={this.state.data[i].title} />)
+        }
         console.log(productList);
 
         return (
@@ -65,11 +82,13 @@ export default class ProductsSlider extends Component {
             <div className="products-slider">
                 {!this.props.notitle && <h3 className="home-section-title">Encuentra los mejores productos en {this.props.category && this.props.category}<Link href={this.props.category && "/categoria/" + this.props.category}><a>Ver todos</a></Link></h3>}
                 <div className="slider-movil">
-
-                    <Slider autoplay={100}>
-                        {productList}
-                    </Slider>
+                    <section className="content-products-slider">
+                        {productLista}
+                    </section>
                 </div>
+                <Slider autoplay={13000}>
+                    {productList}
+                </Slider>
 
             </div>
         )
