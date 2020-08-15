fcf1cfbbe16a94f60a8da2b9454048e5229327e5
Jhoubert Rincon <jhoubert.r@gmail.com>
2020-08-15 16:47:50 -0500

    add environments

--- components/Services/FetchData.js
+++ components/Services/FetchData.js
@@ -1,7 +1,11 @@
 export function FetchData(endPoint, postData, method, headers) {
-    let data
-    let baseURL = "https://dev.kieroapi.net/"
-
+    let data;
+    if (process.env.NODE_ENV !== 'production') {
+        let baseURL = "https://dev.kieroapi.net/"
+      }else{
+        let baseURL = "https://kieroapi.net/"
+      }
+    
     return new Promise((resolve, rejects) => {
         fetch(baseURL + endPoint, {
             method: method,
