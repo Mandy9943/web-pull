b5d680d54ef1d656e299caa41e7952199cb625aa
omar <obello@jidadesarrollos.com>
2020-09-30 13:50:49 -0400

    code

---
+++ pages/notificacion_compra.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from "next/head";
+import PurchaseNotification from '../components/PurchaseNotification/PurchaseNotification';
+import favicon from "../assets/img/favicon.svg";
+
+
+export default function notificacion_compra() {
+    return (
+        <div>
+            <Head>
+                <title>Kiero | Notificacion de compra</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                <meta name="robots" content="index,follow" />
+                <meta name="robots" content="noodp" />
+                <meta name="robots" content="noydir" />
+                <meta name="description" content="Descubre miles de productos al mejor precio. Envios gratis a todo el pais, encuentra lo que buscas en Kiero.co" />
+                <meta name="Keywords" content="Tienda en Línea" />
+                <link rel="icon" href={favicon} type="image/png" />
+            </Head>
+            <PurchaseNotification />
+        </div>
+    )
+}
