486aa459f7ec1c47ed8d11abcac5130c80a3d3b7
n3s7or <nrmorales.93@gmail.com>
2021-03-08 11:39:38 -0500

    Hotfix Comprador. Limpieza de codigo

--- components/Purchases/Purchases.js
+++ components/Purchases/Purchases.js
@@ -14,45 +14,6 @@ import PurchasesDetail from '../PurchasesDetail';
 import OrderItem from '../OrderItem/OrderItem.js';
 import OrdersDetail from '../OrdersDetail/OrdersDetail.js';
 
-const data = {
-    "code": 200,
-    "data": [
-        {
-            "created_since": "2020-06-17T23:16:04.226275",
-            "method_id": 2,
-            "order_id": 7,
-            "product_id": 41479552,
-            "purchase_status": "NUEVA",
-            "quantity": 1,
-            "seller_id": 22,
-            "status": 2,
-            "total": "135400.00",
-            "transaction_id": null,
-            "updated_since": "2021-03-04T00:00:00",
-            "user_id": 5551,
-            "product": {
-                "title": "Dell Inspiron 15 3000 Series, Core i3 HD 16``",
-                "images": [],
-                "category": "Electronica"
-            },
-            "client": {
-                "name": "Francisco",
-                "last_name": "Aro",
-                "phone": 55665566,
-
-            },
-            "rate_purchase_data": {
-                "rate_shop": 5,
-                "created_since": "yesterday",
-                "comments": "Este producto es la hostia"
-            }
-        }
-    ],
-    "error": {},
-    "filters": [],
-    "pagination": {}
-}
-
 
 function processSellData(data) {
     // Esta uncion es para que los datos devueltos por el endpoint /shop/orders/
@@ -69,15 +30,18 @@ function Purchases(props) {
     const [selected, setSelected] = useState();
 
     useEffect(() => {
-        // const endp = props.mode === "sell" ? "/shop/orders?page=1&limit=4" : "/getPurchases?page=1&size=4"
-        // getData(endp, props.user.jwt)
-        //     .then((response) => {
-        //         setPagination(response.data.pagination);
-        //         setPurchases(processSellData(response.data));   // Dirty hack the funciton
-        //     });
+        const endp = props.mode === "sell" ? "/shop/orders?page=1&limit=4" : "/getPurchases?page=1&size=4"
+        getData(endp, props.user.jwt)
+            .then((response) => {
+                setPagination(response.data.pagination);
 
-        setPagination(data.pagination);
-        setPurchases(processSellData(data));   // Dirty hack the funciton
+                // Dirty Hack
+                if (props.mode === 'sell'){
+                    setPurchases(processSellData(response.data))
+                } else {
+                    setPurchases(response.data.purchases);   // Dirty hack the funciton
+                }
+            });
 
         return () => {
             setPagination();
