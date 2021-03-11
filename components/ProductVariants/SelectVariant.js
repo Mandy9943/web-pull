92b15337661f679ea9a5b71eab5a9d83f24d0ae6
n3s7or <nrmorales.93@gmail.com>
2021-03-11 13:58:02 -0500

    Removiendo ruido en codigo, utilizando backend para obtener los variants

--- components/ProductVariants/SelectVariant.js
+++ components/ProductVariants/SelectVariant.js
@@ -1,27 +1,5 @@
 import React from "react";
 
-//     "display_type":"image",
-//         "values":[
-//         {
-//             "available":false,
-//             "image":"https://images-na.ssl-images-amazon.com/images/I/41d3obyVMVL.jpg",
-//             "select":false,
-//             "value":"White"
-//         },
-//         {
-//             "available":false,
-//             "image":"https://images-na.ssl-images-amazon.com/images/I/41TlePgOFzL.jpg",
-//             "select":false,
-//             "value":"Pink"
-//         },
-//         {
-//             "available":false,
-//             "image":"https://images-na.ssl-images-amazon.com/images/I/41YRaWJlJtL.jpg",
-//             "select":false,
-//             "value":"Black"
-//         }
-//     ]
-
 
 export default function SelectVariant({dimensions, select, name}) {
     return (
