bc848446502025448b86d803d50f1eb3ff072ed9
debaran <davidduque.kiero@gmail.com>
2021-05-24 14:19:06 -0500

    Se añadio un segundo input de contraseña en recuperar contraseña, se valido que la misma fuera de 8 caracteres y que ambas contraseñas fueran iguales, se corrigieron tildes en UserAccount, se corrigio el link de navbar de juguetes y de camaras, se añadio una validacion en el cambio de pagina de los filtros de Category la cual retornaba un error

--- components/ResetPass/index.js
+++ components/ResetPass/index.js
@@ -1,52 +1,101 @@
-import React, {useState} from 'react';
-import { useRouter } from 'next/router'
-import Header from "../Common/Header";
-import Footer from "../Common/Footer";
-import "../ForgotPass/ForgotPass.css";
-import {resetPassword} from "../../services/userApi";
+import React, { useState, useRef } from 'react';
+import { useRouter } from 'next/router';
+import Header from '../Common/Header';
+import Footer from '../Common/Footer';
+import '../ForgotPass/ForgotPass.css';
+import { resetPassword } from '../../services/userApi';
 import './ResetPass.css';
+import CheckIcon from '@material-ui/icons/Check';
+import CloseIcon from '@material-ui/icons/Close';
 
-function ResetPass({token}) {
+function ResetPass({ token }) {
+	const router = useRouter();
+	const [password1, setPassword1] = useState();
+	const [password2, setPassword2] = useState();
+	const [equal, setEqual] = useState(false);
+	const [error, setError] = useState();
+	const handlePassword1 = (e) => {
+		setPassword1(e.target.value);
+        if (e.target.value && password2 && e.target.value === password2) {
+			setEqual(true);
+		} else {
+			setEqual(false);
+		}
+	};
+	const handlePassword2 = (e) => {
+		setPassword2(e.target.value);
+		if (e.target.value && password1 && e.target.value === password1) {
+			setEqual(true);
+		} else {
+			setEqual(false);
+		}
+	};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const rs = await resetPassword(password1, token);
+		if (rs !== 1) {
+			setError(rs);
+		} else {
+			if (password1 !== password2) {
+				setError('Las contraseñas no coinciden');
+			} else {
+				router.push('/login');
+			}
+		}
+	};
 
-   const router = useRouter();
-   const [password, setPassword] = useState(); 
-   const [error, setError] = useState(); 
-
-   const handlePassword = e => {
-    setPassword(e.target.value);
-   };
-
-   const handleSubmit = async e => {
-       e.preventDefault();
-       const rs = await resetPassword(password, token);
-       if (rs !== 1) {
-           setError(rs);
-       } else {
-           router.push('/login');
-       }
-   };
-
-    return (
-        <div className="container forgot-component">
-        <Header/>
-        <div className="forgot-pass">
-            <h1>Nueva contraseña</h1>
-            {
-                error && <small>{error}</small>
-            }
-            <form className="form" onSubmit={handleSubmit}>
-                <div className="forgot-pass-content">
-                    <p className="forgot-pass-text">Ingrese la nueva contraseña</p>
-                    <input className="input" type="password" value={password} onChange={handlePassword} placeholder="Contraseña"/>
-                </div>
-                <button type="submit" className="main-button button-recovery">
-                    <p>Cambiar</p>
-                </button>
-            </form>
-        </div>
-        <Footer/>
-    </div>
-    );
+	return (
+		<div className="container forgot-component">
+			<Header />
+			<div className="forgot-pass">
+				<h1>Nueva contraseña</h1>
+				{error && <small>{error}</small>}
+				<form className="form" onSubmit={handleSubmit}>
+					<div className="forgot-pass-content">
+						<p className="forgot-pass-text">Ingrese la nueva contraseña</p>
+						<div className="inputContainer">
+							<input
+								id="passwd1"
+								required
+								minLength={8}
+								className="input"
+								type="password"
+								value={password1}
+								onChange={handlePassword1}
+								placeholder="Contraseña"
+							/>
+							{equal ? (
+								<CheckIcon className="equalityConfirm" />
+							) : (
+								<CloseIcon className="equalityConfirm" />
+							)}
+						</div>
+						<div className="inputContainer">
+							<input
+								id="passwd2"
+								required
+								minLength={8}
+								className="input"
+								type="password"
+								value={password2}
+								onChange={handlePassword2}
+								placeholder="Confirma la contraseña"
+							/>
+							{equal ? (
+								<CheckIcon className="equalityConfirm" />
+							) : (
+								<CloseIcon className="equalityConfirm" />
+							)}
+						</div>
+					</div>
+					<button type="submit" className="main-button button-recovery">
+						<p>Cambiar</p>
+					</button>
+				</form>
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default ResetPass;
