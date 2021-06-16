48f4d42f06c01df83729c09100f1b27a4e65fe01
Gustavo Adolfo Pereira Villa <gapereira@outlook.es>
2021-06-16 16:05:38 -0500

    se modifican estilos en las vistas mobile

--- components/ProductsSlider/ProductsSlider.js
+++ components/ProductsSlider/ProductsSlider.js
@@ -1,105 +1,115 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Link from "next/link";
-import ProductCard from '../ProductCard/ProductCard';
+import ProductCard from "../ProductCard/ProductCard";
 import "./ProductsSlider.css";
-import { getProductsBasic } from "../../services/productsApi"
+import { getProductsBasic } from "../../services/productsApi";
 import Success from "../Login/Success";
-import { getImgUrl } from "../../lib/config"
-import Slider from 'react-animated-slider';
-
+import { getImgUrl } from "../../lib/config";
+import Slider from "react-animated-slider";
 
 export default class ProductsSlider extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			data: [],
+		};
+	}
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-        }
-    }
+	componentDidMount() {
+		getProductsBasic(this.props.category, 25).then((response) => {
+			let data = [];
+			let product;
+			let i = 1;
 
-    componentDidMount() {
-        getProductsBasic(this.props.category, 25)
-            .then((response) => {
-                let data = [];
-                let product;
-                let i = 1;
+			for (product in response.data.results) {
+				data.push(response.data.results[product]);
+			}
+			this.setState({ data });
+		});
+	}
 
-                for (product in response.data.results) {
-                    data.push(response.data.results[product]);
-                }
-                this.setState({ data });
-            });
-        }
+	render() {
+		let productList = [];
+		let productListMobile = [];
+		let tmpList = [];
+		let kid = 0;
+		let skid = 0;
+		for (let i = 0; i < this.state.data.length; i++) {
+			let url = "";
+			if (this.state.data[i].image) {
+				url = getImgUrl(this.state.data[i].image);
+			} else {
+				url =
+					"https://thednetworks.com/wp-content/uploads/2012/01/picture_not_available_400-300.png";
+			}
+			tmpList.push(
+				<ProductCard
+					key={skid++}
+					price={this.state.data[i].price}
+					url={url}
+					product_id={this.state.data[i].product_id}
+					title={this.state.data[i].title}
+				/>
+			);
 
-    render() {
+			if ((i + 1) % 5 === 0 && i > 0) {
+				productList.push(
+					<section key={kid++} className="test">
+						{tmpList}
+					</section>
+				);
+				tmpList = [];
+			}
+		}
 
-        let productList = [];
-        let productListMobile = [];
-        let tmpList = [];
-        let kid = 0;
-        let skid = 0;
-        for (let i=0; i < this.state.data.length; i++) {
-            ;
-            let url = "";
-            if (this.state.data[i].image) {
-                url = getImgUrl(this.state.data[i].image);
-            } else {
-                url = "https://thednetworks.com/wp-content/uploads/2012/01/picture_not_available_400-300.png";
-            }
-            tmpList.push(<ProductCard
-                key={skid++}
-                price={this.state.data[i].price}
-                url={url}
-                product_id={this.state.data[i].product_id}
-                title={this.state.data[i].title} />)
+		if (tmpList.length > 0) {
+			console.log(tmpList);
+			productList.push(
+				<section key={kid++} className="test">
+					{tmpList}
+				</section>
+			);
+		}
 
-            if((i+1)%5===0 && i>0){
-                productList.push(<section key={kid++} className="test">{tmpList}</section>);
-                tmpList = []
-            }
+		for (let i = 0; i < this.state.data.length; i++) {
+			let url = "";
+			if (this.state.data[i].image) {
+				url = getImgUrl(this.state.data[i].image);
+			} else {
+				url =
+					"https://thednetworks.com/wp-content/uploads/2012/01/picture_not_available_400-300.png";
+			}
+			productListMobile.push(
+				<ProductCard
+					key={skid++}
+					price={this.state.data[i].price}
+					url={url}
+					product_id={this.state.data[i].product_id}
+					title={this.state.data[i].title}
+				/>
+			);
+		}
 
-        }
-
-        if(tmpList.length>0){
-            console.log(tmpList);
-            productList.push(<section key={kid++} className="test">{tmpList}</section>)
-        }
-
-
-
-        for (let i = 0; i < this.state.data.length; i++) {
-            let url = "";
-            if (this.state.data[i].image) {
-                url = getImgUrl(this.state.data[i].image);
-            } else {
-                url = "https://thednetworks.com/wp-content/uploads/2012/01/picture_not_available_400-300.png";
-            }
-            productListMobile.push(<ProductCard
-                key={skid++}
-                price={this.state.data[i].price}
-                url={url}
-                product_id={this.state.data[i].product_id}
-                title={this.state.data[i].title} />)
-        }
-
-
-        return (
-
-            <div className="products-slider">
-                {!this.props.notitle && <h3 className="home-section-title">Descubre productos de {this.props.category && this.props.category}
-                <Link href={"/categoria/[category]"} as={this.props.category && "/categoria/" + this.props.category}><a className="accent">Ver todos</a></Link>
-                </h3>
-                }
-                <div className="slider-movil">
-                    <section className="content-products-slider">
-                        {productListMobile}
-                    </section>
-                </div>
-                <Slider autoplay={13000}>
-                    {productList}
-                </Slider>
-
-            </div>
-        )
-    }
+		return (
+			<div className="products-slider">
+				{!this.props.notitle && (
+					<h3 className="home-section-title">
+						Descubre productos de {this.props.category && this.props.category}
+						<Link
+							href={"/categoria/[category]"}
+							as={this.props.category && "/categoria/" + this.props.category}
+						>
+							<a className="accent">Ver todos</a>
+						</Link>
+					</h3>
+				)}
+				<div className="slider-movil">
+					<section className="content-products-slider">
+						{productListMobile}
+					</section>
+				</div>
+				<Slider autoplay={13000}>{productList}</Slider>
+			</div>
+		);
+	}
 }
