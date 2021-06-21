6600de6a908ac90bc053c333886ed35936bb8d2c
Gustavo Adolfo Pereira Villa <gapereira@outlook.es>
2021-06-21 09:27:49 -0500

    actualizacion

--- components/ProductsSlider/ProductsSlider.js
+++ components/ProductsSlider/ProductsSlider.js
@@ -108,7 +108,7 @@ export default class ProductsSlider extends Component {
 						{productListMobile}
 					</section>
 				</div>
-				<Slider autoplay={4000}>{productList}</Slider>
+				<Slider autoplay={5000}>{productList}</Slider>
 			</div>
 		);
 	}
