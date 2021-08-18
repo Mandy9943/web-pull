a1bac066ebc15ad367a95a4007b8f803f473da20
debaran <davidduque.kiero@gmail.com>
2021-08-18 16:06:32 -0500

    se corrigio un error en el check de aceptar terminos y condiciones en PaySection.js que impedia enviar el formulario a payu

--- components/PaySection/PaySection.js
+++ components/PaySection/PaySection.js
@@ -42,9 +42,8 @@ class PaySection extends Component {
 			dataTransaction: [],
 			validForm: true,
 			disabledButton: true,
-			termsOfService: false,
+			termsOfService: '',
 		};
-		this.toggleModalAddr = this.toggleModalAddr.bind(this);
 	}
 	toggleModalAddr() {
 		this.setState({ modalAddr: !this.state.modalAddr });
@@ -334,6 +333,25 @@ class PaySection extends Component {
 		});
 	}
 
+	validateForm = () => {
+		const { user, email, mobile_phone, city, address, termsOfService } = this.state;
+		if (
+			!user ||
+			!email ||
+			!mobile_phone ||
+			!city ||
+			!address
+			// !region ||
+			// !neighborhood
+		) {
+			this.setState({ disabledButton: true, validForm: false });
+		} else {
+			// this.setState({modalAddr: false})
+			this.setState({ disabledButton: false, validForm: true });
+			// this.renderWompi()
+		}
+	};
+
 	handleFormValue = (e) => {
 		let { name, value } = e.target;
 
@@ -346,11 +364,10 @@ class PaySection extends Component {
 		if (name === 'user' || name === 'email' || name === 'address') {
 			this.setState({ [name]: value });
 		}
+		if (name === 'terms') {
+			this.setState({ termsOfService: !this.state.termsOfService });
+		}
 		this.validateForm();
-	};
-
-	handleTerms = (e) => {
-		this.setState({ termsOfService: !this.state.termsOfService });
 	};
 
 	randomPayReference = (length, chars) => {
@@ -409,8 +426,7 @@ class PaySection extends Component {
 		this.state.city == '' ||
 		this.state.region == '' ||
 		this.state.address == '' ||
-		this.state.neighborhood == '' ||
-		this.state.termsOfService == ''
+		this.state.neighborhood == ''
 			? this.setState({ modalAddr: true })
 			: checkout.open(function (result) {
 					var transaction = result.transaction;
@@ -439,28 +455,6 @@ class PaySection extends Component {
 
 					addPaymentDataWompi('/DataWompiTransaction', dataTransaction);
 			  });
-	};
-
-	validateForm = () => {
-		const { user, email, mobile_phone, city, address } = this.state;
-		if (
-			!user ||
-			!email ||
-			!mobile_phone ||
-			!city ||
-			!address ||
-			!termsOfService
-			// !region ||
-			// !neighborhood
-		) {
-			this.setState({ validForm: false });
-			this.setState({ disabledButton: true });
-		} else {
-			// this.setState({modalAddr: false})
-			this.setState({ validForm: true });
-			this.setState({ disabledButton: false });
-			// this.renderWompi()
-		}
 	};
 
 	render() {
@@ -521,11 +515,11 @@ class PaySection extends Component {
 							display: 'flex',
 							marginTop: '40px',
 							height: '60px',
-							borderRadius: '5px',
+							borderRadius: '10px',
 							margin: '10px -80px',
-							justifyContent: 'center',
 							padding: '10px 0px',
-							backgroundColor: '#D4D4D4',
+							backgroundColor: '#E1E1E1',
+							justifyContent: 'center',
 						}}
 					>
 						<div
@@ -535,18 +529,27 @@ class PaySection extends Component {
 						>
 							<Checkbox
 								style={{
-									width: 'fit-content',
 									alignSelf: 'center',
 									marginRight: '10px',
 									color: '#CF0A2C',
 								}}
-								type="checkbox"
+								name="terms"
 								value={this.state.termsOfService}
-								onChange={this.handleTerms}
+								onChange={this.handleFormValue}
 							/>
-							<div>
+							<div
+								style={{
+									fontSize: '15px',
+								}}
+							>
 								Antes de continuar debes aceptar los
-								<div>términos, condiciones y política de privacidad</div>
+								<div
+									style={{
+										color: '#007BFF',
+									}}
+								>
+									términos, condiciones y política de privacidad
+								</div>
 								de KieroMarketPlace
 							</div>
 						</div>
@@ -602,7 +605,7 @@ class PaySection extends Component {
 						<input
 							className="button-finish-pay"
 							onMouseDown={this.validateForm}
-							disabled={this.state.disabledButton}
+							disabled={this.state.termsOfService ? this.state.disabledButton : true}
 							style={{
 								background: this.state.disabledButton ? '#cf0a2c' : '#cf0a2c',
 								color: 'white',
