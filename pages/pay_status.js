4a7017e20ba01362e9e866b30c8774991c5d8475
Julio Xavier <jxarenas21990@gmail.com>
2021-08-25 00:11:59 +0200

    Agregada validación para el caso que no tenga valor la variable

--- pages/pay_status.js
+++ pages/pay_status.js
@@ -23,7 +23,7 @@ function PayStatus({ data, u_data }) {
       setparams(paramsUrl)
       console.log(params)
   }, [router.query]);
-    var listValue = params.extra4.split("~")
+  if(params.extra4!==undefined) var listValue = params.extra4.split("~")
     console.log(params)
     if(params.lapResponseCode == "APPROVED"){
         dataLayer.push({ ecommerce: null });  // Clear the previous ecommerce object.
