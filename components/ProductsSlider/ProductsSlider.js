ef327282e1b0dd984805bd410f9427762c196ec1
julio <julio.arenas@datazucar.cu>
2021-08-29 04:47:22 +0200

    con datos reales

--- components/ProductsSlider/ProductsSlider.js
+++ components/ProductsSlider/ProductsSlider.js
@@ -70,28 +70,7 @@ export default class ProductsSlider extends Component {
 				ecommerce: {
 					currencyCode: "COP",
 					impressions:
-						[
-							{
-								'name': 'Triblend Android T-Shirt',       // Name or ID is required.
-								'id': '12345',
-								'price': '15.25',
-								'brand': 'Google',
-								'category': 'Apparel',
-								'variant': 'Gray',
-								'list': 'Search Results',
-								'position': 1
-							},
-							{
-								'name': 'Donut Friday Scented T-Shirt',
-								'id': '67890',
-								'price': '33.75',
-								'brand': 'Google',
-								'category': 'Apparel',
-								'variant': 'Black',
-								'list': 'Search Results',
-								'position': 2
-							}]
-
+						dataLayerGoogleSlidersUniversal
 				}
 			})
 			// const gtagSlidersUniversal = response.data.results?.map((prod, index) => {
