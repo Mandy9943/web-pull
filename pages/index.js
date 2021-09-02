17989d6b3187441f694be26b7670dbcb9b0c7e7a
julio.arenas <julio.arenas@datazucar.cu>
2021-09-02 18:21:28 +0200

    Trabajando en los temas de klaviyo

--- pages/index.js
+++ pages/index.js
@@ -93,9 +93,6 @@ export default class Index extends Component {
                         }` }}>
 
                     </script>
-                    <script
-                        async type="text/javascript"
-                        src="//static.klaviyo.com/onsite/js/klaviyo.js?company_id=Sr8j85"></script>
 
                 </Head>
                   {/* Google Tag Manager (noscript) */}
