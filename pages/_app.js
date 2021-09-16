a8fa1ec62dbc0d0a9fe76c6dafe246e278758489
rmoscuba <rmoscuba@gmail.com>
2021-09-16 12:41:28 -0400

    Move Google Tag manager snippet to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,23 @@ export default function MyApp({ Component, pageProps}) {
 
 	return (
 		<>
+		
+		{/* Google Tag Manager */}
+		<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-TXNXPM7');`,
+					}}
+		/>
+		<noscript
+				dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXNXPM7" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+				}}
+		/>
+
 		<script
 			dangerouslySetInnerHTML={{ __html: `
 					!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -39,7 +56,6 @@ export default function MyApp({ Component, pageProps}) {
 		/>
 		</noscript>
 
-
 		{/* <!--  Clickcease.com tracking--> */}
 			<script 
 				dangerouslySetInnerHTML={{
@@ -55,20 +71,7 @@ export default function MyApp({ Component, pageProps}) {
 					<a href='https://www.clickcease.com' rel='nofollow'><img src='https://monitor.clickcease.com/stats/stats.aspx' alt='ClickCease'/></a>
 			</noscript>
 		{/* Clickcease.com tracking */}
-               
-		 	<script async src="https://www.googletagmanager.com/gtag/js?id=GTM-TXNXPM7"></script>
-			<script
-				dangerouslySetInnerHTML={{
-				__html: `
-									window.dataLayer = window.dataLayer || [];
-									function gtag(){window.dataLayer.push(arguments);}
-									gtag('js', new Date());
-						
-									gtag('config', 'GTM-TXNXPM7', { page_path: window.location.pathname });
-								
-							`
-				}}
-			/>
+
 			<SocketChat />
 			<SocketUser />
 			<KieroSocketChat />
