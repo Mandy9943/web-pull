7c314ec70fbd09cca210fcf1183ea23396f307eb
Gerson David Hoyos Paternina <gersonhoyos_11@hotmail.com>
2021-09-24 12:35:02 -0500

    change api img

--- components/ListCategory/ListCategory.js
+++ components/ListCategory/ListCategory.js
@@ -305,7 +305,8 @@ class ListCategory extends Component {
 												<div className="product-list-img">
 													<img
 														alt={product.title}
-														src={getImgUrl(product.image)}
+														src={'https://api.kieroapi.net/img/v1/'+ product.product_id + '?img=' + encodeURIComponent(product.image)}
+														// src={getImgUrl(product.image)}
 														className="img"
 													/>
 												</div>
