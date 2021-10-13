efdb0f094807f3e93115d2269edcfaa88865e24d
Gerson David Hoyos Paternina <gersonhoyos_11@hotmail.com>
2021-10-13 10:32:07 -0500

    pull from cesar v2

--- components/Common/ErrorPage/ErrorPage.js
+++ components/Common/ErrorPage/ErrorPage.js
@@ -1,11 +1,19 @@
 import './ErrorPage.css'
 import BackImg from './img/bg.jpg'
+import { faFacebook } from "@fortawesome/free-solid-svg-icons";
 // import 'https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js'
 // import 'https://oss.maxcdn.com/respond/1.4.2/respond.min.js'
 // import './css/font-awesome.min.css'
 
-export default function ErrorPage({ codError}) { 
-
+export default function ErrorPage({ codError, msgError}) { 
+	const fb = "//www.facebook.com/KieroGroup/";
+    const ig = "//www.instagram.com/kierogroup";
+    const tw = "//twitter.com/KIEROGROUP1";
+    const lk = "//www.linkedin.com/in/kiero-group-15a6a7190/";
+    const pt = "//co.pinterest.com/novedadeskiero/pins";
+    const yt = "//www.youtube.com/user/KieroGroup";
+    const ws =
+      "//api.whatsapp.com/send?phone=5715800817&data=Abtjl_JlVp4Y7IoFRWKf-hlWAs-VGJ3oAFmXn4IGwwa3ocqSHrFkuqFrQCSStHVIt7TzEC6yGVjy6TA3FT5FYdPlrLxnmJ5kcvdfZ3tIaO0LElI4BnU3BkZT5O1suCVDqlk&source=FB_Ads&fbclid=IwAR1yaqUnrxYjWvfQ7Ggvz6jWEhP2R0ApqB2GIJcMq7Y52REAaoJCSkBjx00";
 	return (
 		<>
 			<div id="notfound">
@@ -14,14 +22,14 @@ export default function ErrorPage({ codError}) {
 					<div className="notfound-404">
 						<h1>{codError}</h1>
 					</div>
-					<h2>we are sorry, but the page you requested was not found</h2>
-					<a href="#" className="home-btn">Go Home</a>
-					<a href="#" className="contact-btn">Contact us</a>
+					<h2>{msgError}</h2>
+					<a href="/" className="home-btn">Ir a Kiero.co</a>
+					<a href="#" className="contact-btn">Contáctanos</a>
 					<div className="notfound-social">
-						<a href="#"><i className="fa fa-facebook"></i></a>
-						<a href="#"><i className="fa fa-twitter"></i></a>
-						<a href="#"><i className="fa fa-pinterest"></i></a>
-						<a href="#"><i className="fa fa-google-plus"></i></a>
+						<a href={fb}><i className="fa fa-facebook"></i></a>
+						<a href={tw}><i className="fa fa-twitter"></i></a>
+						<a href={pt}><i className="fa fa-pinterest"></i></a>
+						<a href={yt}><i className="fa fa-google-plus"></i></a>
 					</div>
 				</div>
 			</div>
--- pages/_error.js
+++ pages/_error.js
@@ -1,10 +1,11 @@
 import PageError from '../components/Common/ErrorPage'
  
 function Error({ statusCode }) {
+    const msgError = statusCode == 404 ? 'Perdón, la página que buscas no se puede encontrar, te invitamos a regresar a la página principal' : 'Perdón, ha ocurrido un problema por nuestra parte, te invitamos a a regresar a la página principal'
     return (
       <p>
         {statusCode
-          ? <PageError codError={statusCode}/>
+          ? <PageError codError={statusCode} msgError={msgError}/>
           : 'An error occurred on client'}
       </p>
     )
