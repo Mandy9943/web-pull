b3bf35e3d0b1caeb0c6509c1352a9020a6751a4c
Cesar <cesrmartn@yahoo.es>
2021-11-02 13:42:27 -0400

    iniciando estilos de error page

--- components/Common/ErrorPage/ErrorPage.js
+++ components/Common/ErrorPage/ErrorPage.js
@@ -4,7 +4,25 @@ import ImgError from "./img/Gráfico Página de error.svg";
 export default function ErrorPage({ codError, msgError }) {
   return (
     <>
-      <div id="notfound"></div>
+      <div id="notfound">
+        <div className="notfoundWrapper">
+
+        <div className="notfoundImg">
+          
+        </div>
+        <div className="notfoundInfo">
+          <div className="notfoundTitle">
+            
+          </div>
+          <div className="notfoundDescription">
+
+          </div>
+          <div className="notfoundLinks">
+
+          </div>
+        </div>
+      </div>
+        </div>
     </>
   );
 }
