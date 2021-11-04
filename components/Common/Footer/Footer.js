2014b65424bcc3e25907db98505f3e7f09cbc2ce
Cesar <cesrmartn@yahoo.es>
2021-11-04 00:00:48 -0400

    add footer red

--- components/Common/Footer/Footer.js
+++ components/Common/Footer/Footer.js
@@ -13,6 +13,7 @@ export default class Footer extends Component {
     render() {
         let url = "https://www.sic.gov.co/";
         return (
+            <>
             <footer id="footerWrapper" >
                 <div className="footer">
 
@@ -83,6 +84,52 @@ export default class Footer extends Component {
                 </div>
                 </div>
             </footer>
+            <div className="footerRed">
+                <div className="footerRed-content">
+                    <div className="footerRed-tex">
+                        <p>{this.getYear()} Kiero. Todos los derechos reservados.</p>
+                        <ul>
+                            <a href={url} target="_blank">www.sic.gov.co</a>
+                            <Link href="/terminos"><a>Términos y condiciones</a></Link>
+                            <Link href="/privacidad"><a>Políticas de privacidad</a></Link>
+                            <Link href="/contactanos_email"><a>Contáctanos</a></Link>
+                            <Link href="/ayuda"><a>Ayuda / PQR</a></Link>
+                        </ul>
+                    </div>
+                    {/*
+                    <div className="footerRed-download">
+                        <Link href="#">
+                            <a className="download-app">
+                                <img className="phone-img" src={Phone} />
+                                <p>¡Descarga gratis la app!</p>
+                            </a>
+                        </Link>
+                    </div>*/}
+                </div>
+                
+                <div className="footerRed-content footerRed-movil">
+                    {/*
+                    <div className="footerRed-download">
+                        <Link href="#">
+                            <a className="download-app">
+                                <img className="phone-img" src={Phone} />
+                                <p>¡Descarga gratis la app!</p>
+                            </a>
+                        </Link>
+                    </div>*/}
+                    <div className="footerRed-tex">
+                        <ul>
+                            <Link href="/ayuda"><a>Ayuda / PQR</a></Link>
+                            <Link href="/privacidad"><a>Políticas de privacidad</a></Link>
+                            <Link href="/terminos"><a>Términos y condiciones</a></Link>
+                            <Link href="/contactanos_email"><a>Contáctanos</a></Link>
+                            <a href={url} target="_blank">www.sic.gov.co</a>
+                        </ul>
+                        <p>{this.getYear()} Kiero. Todos los derechos reservados.</p>
+                    </div>
+                </div>
+            </div>
+            </>
         )
     }
 }
