2b8e4f23d4dcf2bee690f7e27982436f0a77ef54
Cesar <cesrmartn@yahoo.es>
2021-11-04 00:09:28 -0400

    reomve news component from home

--- components/Common/Footer/Footer.js
+++ components/Common/Footer/Footer.js
@@ -1,135 +1,149 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Phone from "../../../assets/img/phone.png";
 import "./Footer.css";
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 export default class Footer extends Component {
-    getYear() {
-        return new Date().getFullYear();
-    }
-   
-    render() {
-        let url = "https://www.sic.gov.co/";
-        return (
-            <>
-            <footer id="footerWrapper" >
-                <div className="footer">
+  getYear() {
+    return new Date().getFullYear();
+  }
 
-             
-             <div className="footerContentWrapper">
-
-              
-            
-             
-               <div className="footerContent">
-                    <div className="footerElement">
-                        <h3>Acerca de </h3>
-                        <div className="footerLinks">
-                            <Link href="/about">
-                                <a className="footerLink">Sobre kiero</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Prensa</a>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="footerElement">
-                        <h3>Metodos de pago </h3>
-                        <div className="footerLinks">
-                            <Link href="/about">
-                                <a className="footerLink">Deposito Bancario</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Efectivo</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Visa</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Master Card</a>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="footerElement">
-                        <h3>Redes Sociales </h3>
-                        <div className="footerLinks">
-                            <Link href="/about">
-                                <a className="footerLink">Facebook</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Twitter</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Instagram</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Youtube</a>
-                            </Link>
-                        </div>
-                    </div>
-                    <div className="footerElement ">
-                        <h3>Mi cuenta </h3>
-                        <div className="footerLinks">
-                            <Link href="/about">
-                                <a className="footerLink">Ingresar</a>
-                            </Link>
-                            <Link href="/blog">
-                                <a className="footerLink">Registrarse</a>
-                            </Link>
-                        </div>
-                    </div>
-               </div>
+  render() {
+    let url = "https://www.sic.gov.co/";
+    return (
+      <footer>
+        <div id="footerWrapper">
+          <div className="footer">
+            <div className="footerContentWrapper">
+              <div className="footerContent">
+                <div className="footerElement">
+                  <h3>Acerca de </h3>
+                  <div className="footerLinks">
+                    <Link href="/about">
+                      <a className="footerLink">Sobre kiero</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Prensa</a>
+                    </Link>
+                  </div>
                 </div>
+                <div className="footerElement">
+                  <h3>Metodos de pago </h3>
+                  <div className="footerLinks">
+                    <Link href="/about">
+                      <a className="footerLink">Deposito Bancario</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Efectivo</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Visa</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Master Card</a>
+                    </Link>
+                  </div>
                 </div>
-            </footer>
-            <div className="footerRed">
-                <div className="footerRed-content">
-                    <div className="footerRed-tex">
-                        <p>{this.getYear()} Kiero. Todos los derechos reservados.</p>
-                        <ul>
-                            <a href={url} target="_blank">www.sic.gov.co</a>
-                            <Link href="/terminos"><a>Términos y condiciones</a></Link>
-                            <Link href="/privacidad"><a>Políticas de privacidad</a></Link>
-                            <Link href="/contactanos_email"><a>Contáctanos</a></Link>
-                            <Link href="/ayuda"><a>Ayuda / PQR</a></Link>
-                        </ul>
-                    </div>
-                    {/*
-                    <div className="footerRed-download">
-                        <Link href="#">
-                            <a className="download-app">
-                                <img className="phone-img" src={Phone} />
-                                <p>¡Descarga gratis la app!</p>
-                            </a>
-                        </Link>
-                    </div>*/}
+                <div className="footerElement">
+                  <h3>Redes Sociales </h3>
+                  <div className="footerLinks">
+                    <Link href="/about">
+                      <a className="footerLink">Facebook</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Twitter</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Instagram</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Youtube</a>
+                    </Link>
+                  </div>
                 </div>
-                
-                <div className="footerRed-content footerRed-movil">
-                    {/*
-                    <div className="footerRed-download">
-                        <Link href="#">
-                            <a className="download-app">
-                                <img className="phone-img" src={Phone} />
-                                <p>¡Descarga gratis la app!</p>
-                            </a>
-                        </Link>
-                    </div>*/}
-                    <div className="footerRed-tex">
-                        <ul>
-                            <Link href="/ayuda"><a>Ayuda / PQR</a></Link>
-                            <Link href="/privacidad"><a>Políticas de privacidad</a></Link>
-                            <Link href="/terminos"><a>Términos y condiciones</a></Link>
-                            <Link href="/contactanos_email"><a>Contáctanos</a></Link>
-                            <a href={url} target="_blank">www.sic.gov.co</a>
-                        </ul>
-                        <p>{this.getYear()} Kiero. Todos los derechos reservados.</p>
-                    </div>
+                <div className="footerElement ">
+                  <h3>Mi cuenta </h3>
+                  <div className="footerLinks">
+                    <Link href="/about">
+                      <a className="footerLink">Ingresar</a>
+                    </Link>
+                    <Link href="/blog">
+                      <a className="footerLink">Registrarse</a>
+                    </Link>
+                  </div>
                 </div>
+              </div>
             </div>
-            </>
-        )
-    }
+          </div>
+        </div>
+        <div className="footerRed">
+          <div className="footerRed-content">
+            <div className="footerRed-tex">
+              <p>{this.getYear()} Kiero. Todos los derechos reservados.</p>
+              <ul>
+                <a href={url} target="_blank">
+                  www.sic.gov.co
+                </a>
+                <Link href="/terminos">
+                  <a>Términos y condiciones</a>
+                </Link>
+                <Link href="/privacidad">
+                  <a>Políticas de privacidad</a>
+                </Link>
+                <Link href="/contactanos_email">
+                  <a>Contáctanos</a>
+                </Link>
+                <Link href="/ayuda">
+                  <a>Ayuda / PQR</a>
+                </Link>
+              </ul>
+            </div>
+            {/*
+                    <div className="footerRed-download">
+                        <Link href="#">
+                            <a className="download-app">
+                                <img className="phone-img" src={Phone} />
+                                <p>¡Descarga gratis la app!</p>
+                            </a>
+                        </Link>
+                    </div>*/}
+          </div>
+
+          <div className="footerRed-content footerRed-movil">
+            {/*
+                    <div className="footerRed-download">
+                        <Link href="#">
+                            <a className="download-app">
+                                <img className="phone-img" src={Phone} />
+                                <p>¡Descarga gratis la app!</p>
+                            </a>
+                        </Link>
+                    </div>*/}
+            <div className="footerRed-tex">
+              <ul>
+                <Link href="/ayuda">
+                  <a>Ayuda / PQR</a>
+                </Link>
+                <Link href="/privacidad">
+                  <a>Políticas de privacidad</a>
+                </Link>
+                <Link href="/terminos">
+                  <a>Términos y condiciones</a>
+                </Link>
+                <Link href="/contactanos_email">
+                  <a>Contáctanos</a>
+                </Link>
+                <a href={url} target="_blank">
+                  www.sic.gov.co
+                </a>
+              </ul>
+              <p>{this.getYear()} Kiero. Todos los derechos reservados.</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import Link from 'next/link';
-import './Home.css';
-import Social from './../SocialBar';
-import ProductCardFinding from '../Common/ProductCardFinding/ProductCardFinding';
-import Logo1 from '../../assets/img/logo-social.png';
-import Logo2 from '../../assets/img/logo-social1.png';
+import React, { Component } from "react";
+import Link from "next/link";
+import "./Home.css";
+import Social from "./../SocialBar";
+import ProductCardFinding from "../Common/ProductCardFinding/ProductCardFinding";
+import Logo1 from "../../assets/img/logo-social.png";
+import Logo2 from "../../assets/img/logo-social1.png";
 
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
 // import Nav from "../Common/Nav/Nav";
 // import PrincipalSlider from './../PrincipalSlider';
@@ -23,93 +23,94 @@ import dynamic from 'next/dynamic';
 // import News from './../News';
 // import Footer from '../Common/Footer';
 
-const Nav = dynamic(() => import('../Common/Nav/Nav'));
-const PrincipalSlider = dynamic(() => import('./../PrincipalSlider'));
-const Payment = dynamic(() => import('./../PaymentBar'));
-const ProductsSlider = dynamic(() => import('./../ProductsSlider'));
-const SecundarySlider = dynamic(() => import('./../SecundarySlider'));
-const LoginMovil = dynamic(() => import('./../LoginMovil'));
-const Finding = dynamic(() => import('./../Finding'));
-const Explorer = dynamic(() => import('./../Common/Explorer'));
-const ListProductMovil = dynamic(() => import('./../listProductMovil/listProductMovil'));
-const CategoriesImgMenu = dynamic(() => import('./../CategoriesImgMenu'));
-const Tickets = dynamic(() => import('./../Tickets'));
-const Info = dynamic(() => import('./../Info'));
-const News = dynamic(() => import('./../News'));
-const Footer = dynamic(() => import('../Common/Footer'));
+const Nav = dynamic(() => import("../Common/Nav/Nav"));
+const PrincipalSlider = dynamic(() => import("./../PrincipalSlider"));
+const Payment = dynamic(() => import("./../PaymentBar"));
+const ProductsSlider = dynamic(() => import("./../ProductsSlider"));
+const SecundarySlider = dynamic(() => import("./../SecundarySlider"));
+const LoginMovil = dynamic(() => import("./../LoginMovil"));
+const Finding = dynamic(() => import("./../Finding"));
+const Explorer = dynamic(() => import("./../Common/Explorer"));
+const ListProductMovil = dynamic(() =>
+  import("./../listProductMovil/listProductMovil")
+);
+const CategoriesImgMenu = dynamic(() => import("./../CategoriesImgMenu"));
+const Tickets = dynamic(() => import("./../Tickets"));
+const Info = dynamic(() => import("./../Info"));
+const Footer = dynamic(() => import("../Common/Footer"));
 
 export default class Home extends Component {
-	render() {
-		//console.log(this.props.user_data)
-		let u_data = this.props.user_data;
-		let authenticated = this.props.authenticated;
+  render() {
+    //console.log(this.props.user_data)
+    let u_data = this.props.user_data;
+    let authenticated = this.props.authenticated;
 
-		return (
-			<div className="container">
-				<Nav
-					user={u_data.user}
-					role={u_data.role}
-					jwt={u_data.jwt}
-					home={true}
-					authenticated={u_data.authenticated}
-				/>
-				<PrincipalSlider />
-				<div className="home-content">
-					<Payment />
-					<ProductsSlider category={'Computadoras y tabletas'} />
-					<ProductsSlider category={'Cosméticos y maquillaje'} />
-					<SecundarySlider />
-					<LoginMovil user={u_data.user} authenticated={u_data.authenticated} />
-					<Finding
-						category={{
-							father0: 'Electrodomésticos',
-							father1: 'Computación',
-							img0: 'banner-electrodomesticos.webp',
-							img1: 'banner-computacion.webp',
-							sub1: 'Aspiradoras',
-							url1: 'categoria/Aspiradoras-domésticas',
-							sub2: 'Monitores',
-							url2: 'categoria/Monitores-de-computadoras',
-						}}
-					/>
-					<ProductsSlider category={'Juguetes y juegos'} />
-					<Explorer />
-					<ProductsSlider category={'Bebés'} />
-					<Finding
-						category={{
-							father0: 'Deportes',
-							father1: 'Electrónica',
-							img0: 'banner-deportes.webp',
-							img1: 'banner-drones.webp',
-							sub1: 'Bicicletas-de-carretera',
-							url1: 'categoria/Bicicletas',
-							sub2: 'Drones',
-							url2: 'categoria/Cuadricópteros-drones-y-accesorios',
-						}}
-					/>
-					<Explorer />
-					<ProductsSlider category={'Animales y Mascotas'} />
-					<ListProductMovil jwt={u_data.jwt} />
-					<CategoriesImgMenu />
-					<Tickets />
-				</div>
-				<Info />
-				<News />
-				{/*<Social />*/}
-				<Footer />
-				<div className="footer-social">
-					<Link href={'//sic.gov.co'}>
-						<a target="_blank">
-							<img alt="sic.gov.co" src={Logo1} />
-						</a>
-					</Link>
-					<Link href={'//sic.gov.co'}>
-						<a target="_blank">
-							<img alt="sic.gov.co" src={Logo2} />
-						</a>
-					</Link>
-				</div>
-			</div>
-		);
-	}
+    return (
+      <div className="container">
+        <Nav
+          user={u_data.user}
+          role={u_data.role}
+          jwt={u_data.jwt}
+          home={true}
+          authenticated={u_data.authenticated}
+        />
+        <PrincipalSlider />
+        <div className="home-content">
+          <Payment />
+          <ProductsSlider category={"Computadoras y tabletas"} />
+          <ProductsSlider category={"Cosméticos y maquillaje"} />
+          <SecundarySlider />
+          <LoginMovil user={u_data.user} authenticated={u_data.authenticated} />
+          <Finding
+            category={{
+              father0: "Electrodomésticos",
+              father1: "Computación",
+              img0: "banner-electrodomesticos.webp",
+              img1: "banner-computacion.webp",
+              sub1: "Aspiradoras",
+              url1: "categoria/Aspiradoras-domésticas",
+              sub2: "Monitores",
+              url2: "categoria/Monitores-de-computadoras",
+            }}
+          />
+          <ProductsSlider category={"Juguetes y juegos"} />
+          <Explorer />
+          <ProductsSlider category={"Bebés"} />
+          <Finding
+            category={{
+              father0: "Deportes",
+              father1: "Electrónica",
+              img0: "banner-deportes.webp",
+              img1: "banner-drones.webp",
+              sub1: "Bicicletas-de-carretera",
+              url1: "categoria/Bicicletas",
+              sub2: "Drones",
+              url2: "categoria/Cuadricópteros-drones-y-accesorios",
+            }}
+          />
+          <Explorer />
+          <ProductsSlider category={"Animales y Mascotas"} />
+          <ListProductMovil jwt={u_data.jwt} />
+          <CategoriesImgMenu />
+          <Tickets />
+        </div>
+        <Info />
+        {/* <News /> */}
+        {/*<Social />*/}
+        <Footer />
+        <div className="footer-social">
+          <Link href={"//sic.gov.co"}>
+            <a target="_blank">
+              <img alt="sic.gov.co" src={Logo1} />
+            </a>
+          </Link>
+          <Link href={"//sic.gov.co"}>
+            <a target="_blank">
+              <img alt="sic.gov.co" src={Logo2} />
+            </a>
+          </Link>
+        </div>
+      </div>
+    );
+  }
 }
