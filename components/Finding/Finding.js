1f7a0f3afc25e61504a8a648401b20c062832ccb
Maykel Arias Torres <hanibal920915@gmail.com>
2021-11-11 13:35:33 -0500

    Sustituir img por Imagen en  el componente Finding

--- components/Finding/Finding.js
+++ components/Finding/Finding.js
@@ -1,64 +1,63 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Link from "next/link";
 import "./Finding.css";
 import Image from "next/image";
+import Imagen from "../Common/Imagen/Imagen";
 
 class Finding extends Component {
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    let CategoryImg = "https://kiero.co/images/resources/finding-banner/";
-    // let CategoryImg = "//localhost/images/resources/finding-banner/";
+    render() {
+        let CategoryImg = "https://kiero.co/images/resources/finding-banner/";
+        // let CategoryImg = "//localhost/images/resources/finding-banner/";
 
-    return (
-      <div className="wrap-finding">
-        <div className="content-finding">
-          <div className="finding">
-            <div className="wrap-img">
-              <div className="anullProperties">
-                <Image
-                  layout="fill"
-                  src={CategoryImg + this.props.category.img0}
-                  alt={this.props.category.img0}
-                />
-              </div>
+        return (
+            <div className="wrap-finding">
+                <div className="content-finding">
+                    <div className="finding">
+                        <div className="wrap-img">
+                            <div className="anullProperties">
+                                <Imagen
+                                    src={CategoryImg + this.props.category.img0}
+                                    alt={this.props.category.img0}
+                                />
+                            </div>
+                        </div>
+                        <div className="wrap-info">
+                            <p className="category-text">{this.props.category.father0}</p>
+                            <h3 className="finding-title">{this.props.category.sub1}</h3>
+                            <a
+                                href={this.props.category.url1.replace(/ /g, "-").toLowerCase()}
+                            >
+                                Ver más
+                            </a>
+                        </div>
+                    </div>
+                    <div className="finding">
+                        <div className="wrap-img">
+                            <div className="anullProperties">
+                                <Imagen
+                                    src={CategoryImg + this.props.category.img1}
+                                    alt={this.props.category.img1}
+                                />
+                            </div>
+                        </div>
+                        <div className="wrap-info">
+                            <p className="category-text">{this.props.category.father1}</p>
+                            <h3 className="finding-title">{this.props.category.sub2}</h3>
+                            <a
+                                href={this.props.category.url2.replace(/ /g, "-").toLowerCase()}
+                            >
+                                Ver más
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="wrap-info">
-              <p className="category-text">{this.props.category.father0}</p>
-              <h3 className="finding-title">{this.props.category.sub1}</h3>
-              <a
-                href={this.props.category.url1.replace(/ /g, "-").toLowerCase()}
-              >
-                Ver más
-              </a>
-            </div>
-          </div>
-          <div className="finding">
-            <div className="wrap-img">
-              <div className="anullProperties">
-                <Image
-                  layout="fill"
-                  src={CategoryImg + this.props.category.img1}
-                  alt={this.props.category.img1}
-                />
-              </div>
-            </div>
-            <div className="wrap-info">
-              <p className="category-text">{this.props.category.father1}</p>
-              <h3 className="finding-title">{this.props.category.sub2}</h3>
-              <a
-                href={this.props.category.url2.replace(/ /g, "-").toLowerCase()}
-              >
-                Ver más
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default Finding;
