a2d28645723c78f87dbc1b1e526ebe08e22fb9e9
Maykel Arias Torres <hanibal920915@gmail.com>
2021-11-16 20:52:05 -0500

    Agragando modificaciones para actualizar la version del package.json

--- next.config.js
+++ next.config.js
@@ -1,51 +1,52 @@
+const path = require('path')
 
-module.exports = {
-  exportTrailingslash: true,
-  exportPathMap: function() {
-      const paths = {
-          "/": { page: "/" },
-      };
-
-      return paths;
-  }
-}
-
-module.exports = {
-    distDir: "_next",
-    generateBuildId: async () => {
-      if (process.env.BUILD_ID) {
-        return process.env.BUILD_ID;
-      } else {
-        return `${new Date().getTime()}`;
-      }
-    },
-  }
-  
 const withCSS = require('@zeit/next-css');
-const withSass = require('@zeit/next-sass');
+const withSass = require('sass');
 const withImages = require('next-images');
 const withOptimizedImages = require('next-optimized-images');
+const withPlugins = require('next-compose-plugins')
 
-module.exports = withOptimizedImages( withCSS( withSass( {
-  responsive: {
-    adapter: require('responsive-loader/sharp')
-  },
-  module: {
-      rules: [
-          {
-              test: /\.(webp|png|jpg|gif|svg|eot|ttf|woff|woff2)$/i,
-              use: [
-                  {
-                    loader: [
-                      'file-loader',
-                      'webp-loader'
-                    ]
-                  },
-              ],
-          },
-      ],
-  },
-  images: {
-    domains: ['kiero.co', 'api.kieroapi.net', 'images-na.ssl-images-amazon.com']
-  }
-})));
+const nextConfig = {
+    images: {
+        disableStaticImages: true,
+        domains: ['kiero.co', 'api.kieroapi.net', 'images-na.ssl-images-amazon.com', 'localhost', '127.0.0.1']
+    },
+    exportTrailingslash: true,
+    exportPathMap: function () {
+        const paths = {
+            "/": {page: "/"},
+        };
+
+        return paths;
+    },
+    generateBuildId: async () => {
+        if (process.env.BUILD_ID) {
+            return process.env.BUILD_ID;
+        } else {
+            return `${new Date().getTime()}`;
+        }
+    },
+    distDir: "_next",
+    sassOptions: {
+        includePaths: [path.join(__dirname)],
+    },
+    cssOptions: {
+        includePaths: [path.join(__dirname)],
+    },
+}
+const plugins = [
+    [withCSS],
+    [withSass],
+    [withImages],
+    [withOptimizedImages, {
+        responsive: {
+            adapter: require('responsive-loader/sharp')
+        },
+        webpack: (config, options) => {
+            config.optimization.splitChunks.styles = {}
+            return config;
+        }
+    }],
+]
+
+module.exports = withPlugins(plugins, nextConfig)
