d90ebef2dcdb8461c0190fd4cddabfb022d78d28
Cesar <cesrmartn@yahoo.es>
2021-11-30 11:35:50 -0500

    Lazy loading ProductDatailMovil con skeleton

--- components/ProductDetailMobil/ProductDetailMobil.js
+++ components/ProductDetailMobil/ProductDetailMobil.js
@@ -1,48 +1,12 @@
 import dynamic from "next/dynamic";
 import React from "react";
-import { Suspense } from "react";
-import { Fragment } from "react";
-import Skeleton from "react-loading-skeleton";
+
 import Logo1 from "../../assets/img/logo-social.png";
 import Logo2 from "../../assets/img/logo-social1.png";
 
 import "./ProductDetailMobil.css";
-const Nav = dynamic(() => import("../Common/Nav/Nav"), {
-  suspense: true,
-});
-const Footer = dynamic(() => import("../Common/Footer"), {
-  suspense: true,
-});
-
-const SkeletonProductDatail = () => {
-  return (
-    <Fragment>
-      <div className="content-curve-shape mb"></div>
-      <div className="content">
-        <div className="wrapper">
-          <div className="product-img">
-            <Skeleton className="product-datail-skeleton" />
-          </div>
-        </div>
-      </div>
-      <div className="skeleton-envios mb">
-        <Skeleton className="product-datail-skeleton" />
-      </div>
-      <div className="skeleton-pagos mb">
-        <Skeleton className="product-datail-skeleton" />
-      </div>
-      <div className="skeleton-detalles mb">
-        <Skeleton className="product-datail-skeleton" />
-      </div>
-      <div className="skeleton-detalles mb">
-        <Skeleton className="product-datail-skeleton" />
-      </div>
-      <div className="skeleton-others mb">
-        <Skeleton className="product-datail-skeleton" />
-      </div>
-    </Fragment>
-  );
-};
+const Nav = dynamic(() => import("../Common/Nav/Nav"));
+const Footer = dynamic(() => import("../Common/Footer"));
 
 const ProductDetailMobil = ({ user_data, data }) => {
   let urlSic = "https://www.sic.gov.co";
@@ -57,7 +21,7 @@ const ProductDetailMobil = ({ user_data, data }) => {
         home={true}
         authenticated={user_data.authenticated}
       />
-      {SkeletonProductDatail()}
+
       <Footer />
       <div className="footer-social ">
         <a href={urlSic} rel="noopener noreferrer" target="_blank">
---
+++ components/ProductDetailMobil/common/SkeletonProductDatail/SkeletonProductDatail.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Fragment } from "react";
+import Skeleton from "react-loading-skeleton";
+import "./SkeletonProductDatail.css";
+
+const SkeletonProductDatail = () => {
+  return (
+    <div className="skeleton-movil-product-datail">
+      <div className="content-curve-shape mb"></div>
+      <div className="content">
+        <div className="wrapper">
+          <div className="product-img">
+            <Skeleton className="product-datail-skeleton" />
+          </div>
+        </div>
+      </div>
+      <div className="skeleton-envios mb">
+        <Skeleton className="product-datail-skeleton" />
+      </div>
+      <div className="skeleton-pagos mb">
+        <Skeleton className="product-datail-skeleton" />
+      </div>
+      <div className="skeleton-detalles mb">
+        <Skeleton className="product-datail-skeleton" />
+      </div>
+      <div className="skeleton-detalles mb">
+        <Skeleton className="product-datail-skeleton" />
+      </div>
+      <div className="skeleton-others mb">
+        <Skeleton className="product-datail-skeleton" />
+      </div>
+    </div>
+  );
+};
+
+export default SkeletonProductDatail;
