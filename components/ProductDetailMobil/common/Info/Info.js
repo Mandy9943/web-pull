058c411342797ee430c16c0cbaaf123655912090
Maykel Arias Torres <hanibal920915@gmail.com>
2021-12-01 08:59:40 -0500

    [ProductDetailMobil] Agregado la parte de Info

--- components/ProductDetailMobil/common/Info/Info.js
+++ components/ProductDetailMobil/common/Info/Info.js
@@ -18,6 +18,32 @@ const Info = () => {
 
                 </div>
             </div>
+            <div className="info">
+                <div className="icon">
+                    {/* TODO: Poner icono correcto */}
+                    <FontAwesomeIcon icon={faTruck} />
+                </div>
+                <div className="text">
+                    <h3>Compra Protegida</h3>
+                    <p>
+                        En caso de que surja algún problema, te devolveremos el dinero.
+                    </p>
+
+                </div>
+            </div>
+            <div className="info">
+                <div className="icon">
+                    {/* TODO: Poner icono correcto */}
+                    <FontAwesomeIcon icon={faTruck} />
+                </div>
+                <div className="text">
+                    <h3>Garantía del Vendedor</h3>
+                    <p>
+                        Garantía por defecto de fábrica de (1) mes.
+                    </p>
+
+                </div>
+            </div>
         </div>
     );
 }
