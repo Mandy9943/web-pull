2059728e3f5aa91d4c58312bb90f9e43a4e21cca
Maykel Arias Torres <hanibal920915@gmail.com>
2021-12-03 14:59:50 -0500

    [ProductDetails] Agregado panel de Información.

--- components/ProductDetailMobil/ProductDetailMobil.js
+++ components/ProductDetailMobil/ProductDetailMobil.js
@@ -16,54 +16,55 @@ import PayMethod from "./common/PayMethod/PayMethod";
 const Nav = dynamic(() => import("../Common/Nav/Nav"));
 const Footer = dynamic(() => import("../Common/Footer"));
 
-const ProductDetailMobil = ({ user_data, data }) => {
-  let urlSic = "https://www.sic.gov.co";
-  console.log("data", data);
-  return (
-    <div id="productDetailMobil">
-      <div className="Nav">
-        <Nav
-          user={user_data.user}
-          jwt={user_data.jwt}
-          home={true}
-          authenticated={user_data.authenticated}
-        />
-      </div>
-      <div className="content-curve-shape">
-        <div className="curve-shape">
-          <img src={backGround} alt="Red background" />
-        </div>
-        <div className="header-detail">
-          <Header title={data.description} bredCumbs={data.breadcum} />
-        </div>
-        <div className="content-images">
-          <div className="slider">
-            <SwiperSlider images={data.images} altImg={data.title}/>
-          </div>
-        </div>
-      </div>
+const ProductDetailMobil = ({user_data, data}) => {
+    let urlSic = "https://www.sic.gov.co";
+    console.log("data", data);
+    return (
+        <div id="productDetailMobil">
+            <div className="Nav">
+                <Nav
+                    user={user_data.user}
+                    jwt={user_data.jwt}
+                    home={true}
+                    authenticated={user_data.authenticated}
+                />
+            </div>
+            <div className="content-curve-shape">
+                <div className="curve-shape">
+                    <img src={backGround} alt="Red background"/>
+                </div>
+                <div className="header-detail">
+                    <Header title={data.description} bredCumbs={data.breadcum}/>
+                </div>
+                <div className="content-images">
+                    <div className="slider">
+                        <SwiperSlider images={data.images} altImg={data.title}/>
+                    </div>
+                </div>
+            </div>
 
-      <CheckoutProduct price={data.price} stock={0} />
-
-      <Footer />
-      <div className="footer-social ">
-        <a href={urlSic} rel="noopener noreferrer" target="_blank">
-          <img
-            loading="lazy"
-            alt="Superintendencia de Industria y Comercio"
-            src={Logo1}
-          />
-        </a>
-        <a href={urlSic} rel="noopener noreferrer" target="_blank">
-          <img
-            loading="lazy"
-            alt="Superintendencia de Industria y Comercio"
-            src={Logo2}
-          />
-        </a>
-      </div>
-    </div>
-  );
+            <CheckoutProduct price={data.price} stock={0}/>
+            <Info/>
+            <PayMethod/>
+            <Footer/>
+            <div className="footer-social ">
+                <a href={urlSic} rel="noopener noreferrer" target="_blank">
+                    <img
+                        loading="lazy"
+                        alt="Superintendencia de Industria y Comercio"
+                        src={Logo1}
+                    />
+                </a>
+                <a href={urlSic} rel="noopener noreferrer" target="_blank">
+                    <img
+                        loading="lazy"
+                        alt="Superintendencia de Industria y Comercio"
+                        src={Logo2}
+                    />
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default ProductDetailMobil;
