42234a97ca1a25bb4b643a4d7528f52cc6d5d0ef
Maykel Arias Torres <hanibal920915@gmail.com>
2021-12-03 23:51:54 -0500

    [ProductDetails] Agregado el componente de Detail

--- components/ProductDetailMobil/ProductDetailMobil.js
+++ components/ProductDetailMobil/ProductDetailMobil.js
@@ -12,6 +12,7 @@ import SwiperSlider from "./common/SwiperSlider/SwipperSlider";
 import "./ProductDetailMobil.css";
 import Info from "./common/Info/Info";
 import PayMethod from "./common/PayMethod/PayMethod";
+import Detail from "./common/Detail/Detail";
 
 const Nav = dynamic(() => import("../Common/Nav/Nav"));
 const Footer = dynamic(() => import("../Common/Footer"));
@@ -46,6 +47,7 @@ const ProductDetailMobil = ({user_data, data}) => {
             <CheckoutProduct price={data.price} stock={0}/>
             <Info/>
             <PayMethod/>
+            <Detail product={data}/>
             <Footer/>
             <div className="footer-social ">
                 <a href={urlSic} rel="noopener noreferrer" target="_blank">
