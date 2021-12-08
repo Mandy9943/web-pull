32b65b211d9ad99dadd4137c14f744cd7cd2932b
Cesar <cesrmartn@yahoo.es>
2021-12-08 11:10:31 -0500

    RecommendedProducts slider

--- components/ProductDetailMobil/common/RecommendedProducts/RecommendedProducts.js
+++ components/ProductDetailMobil/common/RecommendedProducts/RecommendedProducts.js
@@ -1,32 +1,59 @@
-import React, {useEffect, useState} from 'react';
-import "./RecommendedProducts.module.css"
-import {getProductsBasic} from "../../../../services/productsApi";
-import background from "../../../../assets/img/productDetail/trazado-12@2x.svg"
+import React, { useEffect, useState } from "react";
+import { getProductsBasic } from "../../../../services/productsApi";
+import background from "../../../../assets/img/productDetail/trazado-12@2x.svg";
 
-const RecommendedProducts = ({category}) => {
-    const [products, setProducts] = useState([])
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/swiper-bundle.min.css";
+import "swiper/swiper.scss";
 
-    useEffect(() => {
-        const getProducts = async () => {
-            await getProductsBasic(category.name, 10).then(r => {
-                    setProducts(r.data.results)
-                }
-            )
-        }
-        getProducts();
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper";
 
-    }, [])
-    return (
-        <div id="RecommendedProducts">
-            <img src={background} alt="Red background"/>
-            <div className="cabecera">
-                <h3>Otros Productos Relacionados</h3>
-            </div>
-            <div className="slider">
-                {/* TODO: Colocar el slider acá*/}
-            </div>
-        </div>
-    );
-}
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
+import "./RecommendedProducts.module.css";
+const RecommendedProducts = ({ category }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      await getProductsBasic(category.name, 10).then((r) => {
+        setProducts(r.data.results);
+      });
+    };
+    getProducts();
+  }, []);
+  return (
+    <div id="RecommendedProducts">
+      <img src={background} alt="Red background" />
+      <div className="cabecera">
+        <h3>Otros Productos Relacionados</h3>
+      </div>
+      <div className="slider">
+        <Swiper
+          slidesPerView={2}
+          slidesPerGroup={2}
+          spaceBetween={16}
+          pagination={{
+            clickable: true,
+          }}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
 
 export default RecommendedProducts;
