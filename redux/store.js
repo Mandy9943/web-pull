14d32f27763efc0465798d7a110a65366f9df855
Maykel Arias Torres <maykel.t@kiero.co>
2022-01-13 12:35:53 -0500

    Added whatsappReducer.js

--- redux/store.js
+++ redux/store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import payReducer from "./feature/pay/paySlice";
+import whatsappReducer from "./feature/whatsapp/whatsappReducer";
 
 export function makeStore() {
   return configureStore({
-    reducer: { pay: payReducer },
+    reducer: { pay: payReducer, whatsapp: whatsappReducer },
     devTools: process.env.NODE_ENV !== "production",
   });
 }
