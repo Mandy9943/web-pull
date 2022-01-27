e72dee079ccccf46c81a38fb283936be0f9da614
Cesar <cesrmartn@yahoo.es>
2022-01-27 15:21:50 -0500

    Shadow to Images Component and checkproductDesk in product detail desktop

--- components/NewProductDetail/common/CheckoutProductDesk/CheckoutProductDesk.js
+++ components/NewProductDetail/common/CheckoutProductDesk/CheckoutProductDesk.js
@@ -1,9 +1,21 @@
+import { Paper } from "@mui/material";
 import React from "react";
 import CheckoutButton from "../CheckoutButton/CheckoutButton";
 import Counter from "../Counter/Counter";
 import PayMethod from "../PayMethod/PayMethod";
 import WhatsappBanner from "../WhatsappBanner/WhatsappBanner";
 import "./CheckoutProductDesk.module.css";
+import { styled } from "@mui/material/styles";
+const CheckoutProductDeskWrapper = styled(Paper)(() => ({
+  width: "100%",
+  boxSizing: "border-box",
+  borderRadius: "10px",
+  justifyContent: "space-between",
+  padding: "30px 20px",
+  marginBottom: "12px",
+  position: "relative",
+}));
+
 const CheckoutProductDesk = ({
   title,
   price,
@@ -14,61 +26,63 @@ const CheckoutProductDesk = ({
   const count = Math.ceil(Math.random() * 50);
   return (
     <div id="CheckoutProductDesk">
-      <div className="title">
-        <h1>{title.length > 80 ? title.substr(0, 80) + "..." : title}</h1>
-      </div>
-      <span className="oldPrice">
-        ${" "}
-        {parseInt(
-          price / (1 - parseFloat("." + discount_percentage).toFixed(2))
-        )
-          .toString()
-          .split(".")[0]
-          .replace(/(.)(?=(\d{3})+$)/g, "$1.")}
-      </span>
-      <div className="price">
-        <span className="realPrice">
+      <CheckoutProductDeskWrapper elevation={6}>
+        <div className="title">
+          <h1>{title.length > 80 ? title.substr(0, 80) + "..." : title}</h1>
+        </div>
+        <span className="oldPrice">
           ${" "}
-          {price
+          {parseInt(
+            price / (1 - parseFloat("." + discount_percentage).toFixed(2))
+          )
             .toString()
             .split(".")[0]
-            .replace(/(.)(?=(\d{3})+$)/g, "$1.")}{" "}
-          {/* <p>COP</p> */}
+            .replace(/(.)(?=(\d{3})+$)/g, "$1.")}
         </span>
-        <span className="percent">
-          -{parseFloat(discount_percentage).toFixed(0)}% OFF
-        </span>
-      </div>
-      <div className="count">
-        <div className="countText">
-          <h3>Solo quedan</h3>
-          {stock > 0 ? (
-            <p>{stock} en stock </p>
-          ) : (
-            <p className="noAvilable">(Stock Agotado)</p>
-          )}
+        <div className="price">
+          <span className="realPrice">
+            ${" "}
+            {price
+              .toString()
+              .split(".")[0]
+              .replace(/(.)(?=(\d{3})+$)/g, "$1.")}{" "}
+            {/* <p>COP</p> */}
+          </span>
+          <span className="percent">
+            -{parseFloat(discount_percentage).toFixed(0)}% OFF
+          </span>
         </div>
-        <div className="countCounter">
-          <Counter stock={stock} />
+        <div className="count">
+          <div className="countText">
+            <h3>Solo quedan</h3>
+            {stock > 0 ? (
+              <p>{stock} en stock </p>
+            ) : (
+              <p className="noAvilable">(Stock Agotado)</p>
+            )}
+          </div>
+          <div className="countCounter">
+            <Counter stock={stock} />
+          </div>
         </div>
-      </div>
-      <div className="buyButton">
-        <CheckoutButton
-          text="Comprar"
-          disabled={stock <= 0}
-          onClick={onClickBuy}
-          rounded
-        />
-      </div>
-      <div className="purchased">
-        <p>
-          {" "}
-          <span className="number">{count}</span> Personas han comprado este
-          producto recientemente.
-        </p>
-      </div>
-      <PayMethod isDektop />
-      <WhatsappBanner isDesktop />
+        <div className="buyButton">
+          <CheckoutButton
+            text="Comprar"
+            disabled={stock <= 0}
+            onClick={onClickBuy}
+            rounded
+          />
+        </div>
+        <div className="purchased">
+          <p>
+            {" "}
+            <span className="number">{count}</span> Personas han comprado este
+            producto recientemente.
+          </p>
+        </div>
+        <PayMethod isDektop />
+        <WhatsappBanner isDesktop />
+      </CheckoutProductDeskWrapper>
     </div>
   );
 };
--- components/NewProductDetail/common/ProductImageDesk/ProductImageDesk.js
+++ components/NewProductDetail/common/ProductImageDesk/ProductImageDesk.js
@@ -4,8 +4,17 @@ import Image from "next/image";
 import "./ProductImageDesk.module.css";
 import { styled } from "@mui/material/styles";
 import { Paper } from "@mui/material";
-const ActualImg = styled(Paper)(({ theme }) => ({
+const ActualImg = styled(Paper)(() => ({
   borderRadius: "10px",
+}));
+const ProductImgDesk = styled(Paper)(() => ({
+  width: "100%",
+  boxSizing: "border-box",
+  borderRadius: "10px",
+  display: "flex",
+  justifyContent: "space-between",
+  padding: "50px 1.5vw 50px 4.5vw",
+  marginBottom: "12px",
 }));
 
 const ProductImageDesk = ({ images, altImg }) => {
@@ -15,46 +24,48 @@ const ProductImageDesk = ({ images, altImg }) => {
   };
   return (
     <div className="ProductImageDesk">
-      <div className="deskImages">
-        {images.slice(0, 6).map((image) => {
-          return (
-            <div
-              className={`content ${
-                actualImage === image.url && "selectedContent"
-              }`}
-              key={image.url}
-              onClick={() => handleClickImage(image.url)}
-            >
-              <div className="anullProperties">
-                <Image
-                  layout="fill"
-                  data-src={image.url}
-                  src={image.url}
-                  alt={"Producto de kiero " + altImg.substr(0, 80)}
-                />
+      <ProductImgDesk elevation={6}>
+        <div className="deskImages">
+          {images.slice(0, 6).map((image) => {
+            return (
+              <div
+                className={`content ${
+                  actualImage === image.url && "selectedContent"
+                }`}
+                key={image.url}
+                onClick={() => handleClickImage(image.url)}
+              >
+                <div className="anullProperties">
+                  <Image
+                    layout="fill"
+                    data-src={image.url}
+                    src={image.url}
+                    alt={"Producto de kiero " + altImg.substr(0, 80)}
+                  />
+                </div>
               </div>
+            );
+          })}
+        </div>
+        <DivindingLine
+          width="2px"
+          height="auto"
+          margin="0 0 0 4vw"
+          color="#D1D1D1"
+        />
+        <div className="actualImageWrapper">
+          <ActualImg elevation={6}>
+            <div className="anullProperties">
+              <Image
+                layout="fill"
+                data-src={actualImage}
+                src={actualImage}
+                alt={"Producto de kiero " + altImg.substr(0, 80)}
+              />
             </div>
-          );
-        })}
-      </div>
-      <DivindingLine
-        width="2px"
-        height="auto"
-        margin="0 0 0 4vw"
-        color="#D1D1D1"
-      />
-      <div className="actualImageWrapper">
-        <ActualImg elevation={6}>
-          <div className="anullProperties">
-            <Image
-              layout="fill"
-              data-src={actualImage}
-              src={actualImage}
-              alt={"Producto de kiero " + altImg.substr(0, 80)}
-            />
-          </div>
-        </ActualImg>
-      </div>
+          </ActualImg>
+        </div>
+      </ProductImgDesk>
     </div>
   );
 };
