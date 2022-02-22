60da3a054d4f209b41c879ed409f2c3b1d3ce572
Gerson David Hoyos Paternina <gersonhoyos_11@hotmail.com>
2022-02-22 15:47:11 -0500

    pull from maykel

--- components/NewProductDetail/common/CheckoutProductDesk/CheckoutProductDesk.js
+++ components/NewProductDetail/common/CheckoutProductDesk/CheckoutProductDesk.js
@@ -24,107 +24,67 @@ const CheckoutProductDesk = ({
   discount_percentage,
   onClickBuy,
 }) => {
-  const { refHead } =
-    useParallax < HTMLDivElement > { speed: 10, translateY: [-100, 100] };
   const count = Math.ceil(Math.random() * 50);
-  const [dataGpsCheckout, setDataGpsCheckout] = useState(0);
-  const [dataGpsFooter, setDataGpsFooter] = useState(0);
-  const [progress, setProgress] = useState(0);
-  const [enter, setEntered] = useState(0);
-  useEffect(() => {
-    const scrollCheckout = document.querySelector("#CheckoutProductDesk");
-    const scrollFooter = document.querySelector(".containerInfo");
 
-    // scrollDiv.addEventListener("scroll", event => {
-    //   console.log("Top",scrollDiv.scrollTop,"Bottom",scrollDiv.scrollBottom)
-    // }, { passive: true });
-    // const functionDataScroll = () => {
-    //   let coordsCheckout = scrollCheckout.getBoundingClientRect();
-    //   let coordsFooter = scrollFooter.getBoundingClientRect();
-    //   setDataGpsCheckout(coordsCheckout.top);
-    //   setDataGpsFooter(coordsFooter.top);
-    // };
-    // functionDataScroll();
-
-    // window.addEventListener("scroll", functionDataScroll);
-    // return () => {
-    //   window.removeEventListener("scroll", functionDataScroll);
-    // };
-  }, []);
-
-  console.log(
-    "checkout",
-    Math.round(dataGpsCheckout),
-    "footer",
-    Math.round(dataGpsFooter)
-  );
-  console.log(progress, enter);
   return (
-    <Parallax
-      onProgressChange={(progress) => setProgress(progress)}
-      onEnter={() => setEntered(true)}
-      onExit={() => setEntered(false)}
-      // speed={-5}
-      // translateY={[0, 0]}
-    >
-      <CheckoutProductDeskWrapper elevation={4} id="CheckoutProductDesk">
-        <div className="title">
-          <h1>{title.length > 80 ? title.substr(0, 80) + "..." : title}</h1>
-        </div>
-        <span className="oldPrice">
+    <CheckoutProductDeskWrapper elevation={4} id="CheckoutProductDesk">
+      <div className="title">
+        <h1>{title.length > 80 ? title.substr(0, 80) + "..." : title}</h1>
+      </div>
+      <span className="oldPrice">
+        ${" "}
+        {parseInt(
+          price / (1 - parseFloat("." + discount_percentage).toFixed(2))
+        )
+          .toString()
+          .split(".")[0]
+          .replace(/(.)(?=(\d{3})+$)/g, "$1.")}
+      </span>
+      <div className="price">
+        <span className="realPrice">
           ${" "}
-          {parseInt(
-            price / (1 - parseFloat("." + discount_percentage).toFixed(2))
-          )
+          {price
             .toString()
             .split(".")[0]
-            .replace(/(.)(?=(\d{3})+$)/g, "$1.")}
+            .replace(/(.)(?=(\d{3})+$)/g, "$1.")}{" "}
+          {/* <p>COP</p> */}
         </span>
-        <div className="price">
-          <span className="realPrice">
-            ${" "}
-            {price
-              .toString()
-              .split(".")[0]
-              .replace(/(.)(?=(\d{3})+$)/g, "$1.")}{" "}
-            {/* <p>COP</p> */}
-          </span>
-          <span className="percent">
-            -{parseFloat(discount_percentage).toFixed(0)}% OFF
-          </span>
+        <span className="percent">
+          -{parseFloat(discount_percentage).toFixed(0)}% OFF
+        </span>
+      </div>
+      <div className="count">
+        <div className="countText">
+          <h3>Solo quedan</h3>
+          {stock > 0 ? (
+            <p>{stock} en stock </p>
+          ) : (
+            <p className="noAvilable">(Stock Agotado)</p>
+          )}
         </div>
-        <div className="count">
-          <div className="countText">
-            <h3>Solo quedan</h3>
-            {stock > 0 ? (
-              <p>{stock} en stock </p>
-            ) : (
-              <p className="noAvilable">(Stock Agotado)</p>
-            )}
-          </div>
-          <div className="countCounter">
-            <Counter stock={stock} />
-          </div>
+        <div className="countCounter">
+          <Counter stock={stock} />
         </div>
-        <div className="buyButton">
-          <CheckoutButton
-            text="Comprar"
-            disabled={stock <= 0}
-            onClick={onClickBuy}
-            rounded
-          />
-        </div>
-        <div className="purchased">
-          <p>
-            {" "}
-            <span className="number">{count}</span> Personas han comprado este
-            producto recientemente.
-          </p>
-        </div>
-        <PayMethod isDektop />
-        <WhatsappBanner isDesktop />
-      </CheckoutProductDeskWrapper>
-    </Parallax>
+      </div>
+      <div className="buyButton">
+        <CheckoutButton
+          text="Comprar"
+          disabled={stock <= 0}
+          onClick={onClickBuy}
+          rounded
+        />
+      </div>
+      <div className="purchased">
+        <p>
+          {" "}
+          <span className="number">{count}</span> Personas han comprado este
+          producto recientemente.
+        </p>
+      </div>
+      <PayMethod isDektop />
+      <WhatsappBanner isDesktop />
+    </CheckoutProductDeskWrapper>
+    // </Parallax>
   );
 };
 
